3ead5779eb0a585a3f1c08bb408f50eb1b285e75
Akaza Renn <8340896+AkazaRenn@users.noreply.github.com>
2024-05-25 18:34:52 -0400

    Basic code for updating, need a scheduler

--- src/index.tsx
+++ src/index.tsx
@@ -1,222 +1,222 @@
 /// <reference types="../typings/index.d.ts"/>
 
 import {
-  definePlugin,
-  PanelSection,
-  PanelSectionRow,
-  Router,
-  ServerAPI,
-  staticClasses,
-  ToggleField,
-  ButtonItem,
-  ProgressBar,
-  Spinner,
+    definePlugin,
+    PanelSection,
+    PanelSectionRow,
+    Router,
+    ServerAPI,
+    staticClasses,
+    ToggleField,
+    ButtonItem,
+    Spinner,
 } from "decky-frontend-lib";
 import { PyInterop } from "./PyInterop";
 import { useEffect, useState, VFC } from "react";
 import { FaBuffer } from "react-icons/fa";
 import { FlatpakInfo } from "./FlatpakInfo";
 import { CMsgSystemUpdateState } from "../protobuf/build/steammessages_client_objects_pb";
+import { EUpdaterState } from "../protobuf/build/enums_pb";
 
 type FlatpaksDictionary = {
-  [key: string]: FlatpakInfo
+    [key: string]: FlatpakInfo
 }
 
 enum UpdateCheckerState {
-  IDLE = 0,
-  CHECKING,
+    IDLE = 0,
+    CHECKING,
 }
+
+// Global variables
+var task: any = null;
+var updateStateChangeRegistration: any = null;
 
 var paksToUpdate: FlatpaksDictionary = {};
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({ }) => {
-  const [flatPaks, setUpdatableFlatpaks] = useState<FlatpaksDictionary>({});
-  const [_, reloadGUI] = useState<any>("");
-  const [updaterState, setUpdaterState] = useState<UpdateCheckerState>(UpdateCheckerState.IDLE);
-  if (Object.values(flatPaks).length == 0) {
-    paksToUpdate = {};
-  }
-
-  if (updaterState == UpdateCheckerState.CHECKING) {
-    async () => {
-      PyInterop.getUpdatableFlatpaks()
-        .then(data => {
-          if (data.success) {
-            setUpdatableFlatpaks(data.result);
-          }
-        });
+    const [flatPaks, setUpdatableFlatpaks] = useState<FlatpaksDictionary>({});
+    const [_, reloadGUI] = useState<any>("");
+    const [updaterState, setUpdaterState] = useState<UpdateCheckerState>(UpdateCheckerState.IDLE);
+    if (Object.values(flatPaks).length == 0) {
+        paksToUpdate = {};
     }
-  }
 
-  SteamClient.Updates.RegisterForUpdateStateChanges((protoMsg: Uint8Array) => {
-    console.log(CMsgSystemUpdateState.deserializeBinary(protoMsg).toObject());
-  });
-
-  useEffect(() => {
     if (updaterState == UpdateCheckerState.CHECKING) {
-      PyInterop.getUpdatableFlatpaks()
-        .then(data => {
-          if (data.success) {
-            setUpdatableFlatpaks(data.result);
-          }
-        });
-    }
-  }, [])
-
-  return (
-    <PanelSection title="Flatpaks">
-      <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <ButtonItem bottomSeparator="none" onClick={() => {
-            setUpdatableFlatpaks({});
-            setUpdaterState(UpdateCheckerState.CHECKING);
+        async () => {
             PyInterop.getUpdatableFlatpaks()
-              .then(data => {
-                if (data.success) {
-                  setUpdaterState(UpdateCheckerState.IDLE);
-                  setUpdatableFlatpaks(data.result);
-                }
-              });
-          }} disabled={updaterState != UpdateCheckerState.IDLE}>
-            {updaterState == UpdateCheckerState.IDLE &&
-              "Check for updates"
-            }
-            {updaterState == UpdateCheckerState.CHECKING &&
-              "Checking for updates"
-            }
-          </ButtonItem>
-        </div>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          {Object.values(flatPaks).length > 0 &&
-            <div>
-              <b>{Object.values(flatPaks).length}</b> updates available
-            </div>
-          }
-          {updaterState == UpdateCheckerState.CHECKING &&
-            <div>
-              <Spinner width="96px"  height="96px"/>
-            </div>
-          }
-        </div>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <div>
-          {
-            Object.values(flatPaks).map((info) => (
-              <div style={{ display: "block", justifyContent: "stretch" }}>
-                <ToggleField
-                  checked={paksToUpdate[info.appID] !== undefined}
-                  label={info.name}
-                  onChange={(checked: boolean) => {
-                    if (checked) {
-                      paksToUpdate[info.appID] = info;
-                      console.info('added ' + info.appID)
-                      reloadGUI("Added package to update " + info.name)
-                    } else {
-                      delete paksToUpdate[info.appID];
-                      console.info('removed ' + info.appID)
-                      reloadGUI("Removed package to update " + info.name)
+                .then(data => {
+                    if (data.success) {
+                        setUpdatableFlatpaks(data.result);
                     }
-                  }} />
-              </div>
-            ))
-          }
-        </div>
-      </PanelSectionRow>
-
-      {Object.values(flatPaks).length > 0 &&
-        <PanelSectionRow>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <ButtonItem bottomSeparator="none" onClick={() => {
-              setUpdatableFlatpaks({});
-              Router.CloseSideMenus();
-              Router.Navigate("/apply-updates");
-            }} disabled={Object.values(paksToUpdate).length == 0}>Update selected</ButtonItem>
-          </div>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <ButtonItem bottomSeparator="none" onClick={() => {
-              paksToUpdate = flatPaks;
-              setUpdatableFlatpaks({});
-              Router.CloseSideMenus();
-              Router.Navigate("/apply-updates");
-            }}>Update all</ButtonItem>
-          </div>
-        </PanelSectionRow>
-      }
-    </PanelSection>
-  );
-};
-
-const ApplyUpdates: VFC = () => {
-  const paksToUpdateList: FlatpakInfo[] = Object.values(paksToUpdate)
-  const info: FlatpakInfo | undefined = paksToUpdateList.length > 0 ? paksToUpdateList[0] : undefined
-  const [count, setCount] = useState<number>(0);
-  const [totalToUpdate, _] = useState<number>(paksToUpdateList.length)
-
-  if (info) {
-    PyInterop.updateFlatpak(info.appID, false)
-      .then(data => {
-        if (data.success) {
-          delete paksToUpdate[info.appID]
-          setCount(count + 1);
+                });
         }
-      });
-  }
+    }
 
-  return (
-    <PanelSection title="Updating">
-      {info &&
-        <PanelSectionRow>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            Updating {info.name} <i>({info.appID})</i>
-          </div>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <ProgressBar nProgress={(count / totalToUpdate) * 100} />
-          </div>
-          <div>
-            <div style={{ display: "block", justifyContent: "normal" }}>
-              <h2>Flatpaks to update</h2>
-            </div>
-            {
-              paksToUpdateList.map((i) => (
-                <div style={{ display: "block", justifyContent: "normal" }}>
-                  {i.name}
+    useEffect(() => {
+        if (updaterState == UpdateCheckerState.CHECKING) {
+            PyInterop.getUpdatableFlatpaks()
+                .then(data => {
+                    if (data.success) {
+                        setUpdatableFlatpaks(data.result);
+                    }
+                });
+        }
+    }, [])
+
+    return (
+        <PanelSection title="Flatpaks">
+            <PanelSectionRow>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <ButtonItem bottomSeparator="none" onClick={() => {
+                        setUpdatableFlatpaks({});
+                        setUpdaterState(UpdateCheckerState.CHECKING);
+                        PyInterop.getUpdatableFlatpaks()
+                            .then(data => {
+                                if (data.success) {
+                                    setUpdaterState(UpdateCheckerState.IDLE);
+                                    setUpdatableFlatpaks(data.result);
+                                }
+                            });
+                    }} disabled={updaterState != UpdateCheckerState.IDLE}>
+                        {updaterState == UpdateCheckerState.IDLE &&
+                            "Check for updates"
+                        }
+                        {updaterState == UpdateCheckerState.CHECKING &&
+                            "Checking for updates"
+                        }
+                    </ButtonItem>
                 </div>
-              ))
+            </PanelSectionRow>
+
+            <PanelSectionRow>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    {Object.values(flatPaks).length > 0 &&
+                        <div>
+                            <b>{Object.values(flatPaks).length}</b> updates available
+                        </div>
+                    }
+                    {updaterState == UpdateCheckerState.CHECKING &&
+                        <div>
+                            <Spinner width="96px" height="96px" />
+                        </div>
+                    }
+                </div>
+            </PanelSectionRow>
+
+            <PanelSectionRow>
+                <div>
+                    {
+                        Object.values(flatPaks).map((info) => (
+                            <div style={{ display: "block", justifyContent: "stretch" }}>
+                                <ToggleField
+                                    checked={paksToUpdate[info.appID] !== undefined}
+                                    label={info.name}
+                                    onChange={(checked: boolean) => {
+                                        if (checked) {
+                                            paksToUpdate[info.appID] = info;
+                                            console.info('added ' + info.appID)
+                                            reloadGUI("Added package to update " + info.name)
+                                        } else {
+                                            delete paksToUpdate[info.appID];
+                                            console.info('removed ' + info.appID)
+                                            reloadGUI("Removed package to update " + info.name)
+                                        }
+                                    }} />
+                            </div>
+                        ))
+                    }
+                </div>
+            </PanelSectionRow>
+
+            {Object.values(flatPaks).length > 0 &&
+                <PanelSectionRow>
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                        <ButtonItem bottomSeparator="none" onClick={() => {
+                            setUpdatableFlatpaks({});
+                            Router.CloseSideMenus();
+                            Router.Navigate("/apply-updates");
+                        }} disabled={Object.values(paksToUpdate).length == 0}>Update selected</ButtonItem>
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                        <ButtonItem bottomSeparator="none" onClick={() => {
+                            paksToUpdate = flatPaks;
+                            setUpdatableFlatpaks({});
+                            Router.CloseSideMenus();
+                            Router.Navigate("/apply-updates");
+                        }}>Update all</ButtonItem>
+                    </div>
+                </PanelSectionRow>
             }
-          </div>
-        </PanelSectionRow>
-      }
-      {info === undefined && count > 0 &&
-        <PanelSectionRow>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <i>Updated <b>{count}</b> flatpaks!</i>
-          </div>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <ProgressBar nProgress={(count / totalToUpdate) * 100} />
-          </div>
-        </PanelSectionRow>
-      }
-    </PanelSection >
-  );
+        </PanelSection>
+    );
 };
+
+function unregisterUpdateStateChangeRegistration(): void {
+    if (updateStateChangeRegistration) {
+        updateStateChangeRegistration.unregister();
+    }
+}
+
+function checkForUpdates(): void {
+    console.log("Checking for updates...");
+    // updateStateChangeRegistration = SteamClient.Updates.RegisterForUpdateStateChanges(updateStateChangeHandler);
+    // SteamClient.Updates.CheckForUpdates().then((_) => {
+    //     console.log("Got updates");
+    // }).catch((_) => {
+    //     unregisterUpdateStateChangeRegistration();
+    // })
+};
+
+function updateStateChangeHandler(protoMsg: Uint8Array): void {
+    try {
+        var updateState = CMsgSystemUpdateState.deserializeBinary(protoMsg).toObject();
+    } catch (e) {
+        console.log(e);
+        unregisterUpdateStateChangeRegistration();
+        return;
+    }
+
+    switch (updateState.state) {
+        case EUpdaterState.K_EUPDATERSTATE_AVAILABLE:
+            console.log("Updates available, applying...");
+            // Not sure what 'CAI=' means but it works...
+            SteamClient.Updates.ApplyUpdates('CAI=');
+            break;
+        case EUpdaterState.K_EUPDATERSTATE_SYSTEMRESTARTPENDING:
+            if (updateState.supportsOsUpdates) {
+                console.log("System restart pending, restarting...");
+                SteamClient.System.RestartPC();
+            } else {
+                console.log("Invalid state, system restart available but OS updates are unsupported...");
+            }
+            break;
+        case EUpdaterState.K_EUPDATERSTATE_CLIENTRESTARTPENDING:
+            console.log("Client restart pending, restarting...");
+            SteamClient.User.StartRestart();
+            break;
+        case EUpdaterState.K_EUPDATERSTATE_CHECKING:
+        case EUpdaterState.K_EUPDATERSTATE_APPLYING:
+            break;
+        default:
+            console.log("No updates available");
+            unregisterUpdateStateChangeRegistration();
+            break;
+    }
+}
 
 export default definePlugin((serverApi: ServerAPI) => {
-  PyInterop.setServer(serverApi);
-  serverApi.routerHook.addRoute("/apply-updates", ApplyUpdates, {
-    exact: true,
-  });
+    PyInterop.setServer(serverApi);
+    // task = cron.schedule("* 18 * * * a", checkForUpdates);
 
-  return {
-    title: <div className={staticClasses.Title}>Flatpak Updater</div>,
-    content: <Content serverAPI={serverApi} />,
-    icon: <FaBuffer />,
-    onDismount() {
-      serverApi.routerHook.removeRoute("/apply-updates");
-    },
-  };
+    return {
+        title: <div className={staticClasses.Title}>Auto Update</div>,
+        content: <Content serverAPI={serverApi} />,
+        icon: <FaBuffer />,
+        onDismount() {
+            unregisterUpdateStateChangeRegistration();
+            if (task) {
+                task.stop();
+            }
+        },
+    };
 });
